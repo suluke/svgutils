5ce74ef301d5a0ead034f1296275765bd9fee64c
Lukas Böhm <suluke93@gmail.com>
2018-11-17 21:07:30 +0100

    [svg_reader_writer] Improve parser

--- include/svgutils/svg_reader_writer.h
+++ include/svgutils/svg_reader_writer.h
@@ -7,10 +7,27 @@ namespace svg {
 using instream_t = std::istream;
 
 struct SVGReaderWriterBase {
+  struct ParseError {
+    ParseError(std::string what) : what(std::move(what)){};
+    ParseError(const char *what) : /* I said */ what(what){};
+    ParseError(ParseError &&) = default;
+    ParseError &operator=(ParseError &&) = default;
+    ParseError(const ParseError &) = default;
+    ParseError &operator=(const ParseError &) = default;
+    friend inline outstream_t &operator<<(outstream_t &os,
+                                          const ParseError &err) {
+      os << err.what;
+      return os;
+    }
+    std::string what;
+  };
+  using MaybeError = std::optional<ParseError>;
+
   SVGReaderWriterBase(WriterConcept &writer) : writer(writer) {}
-  bool parse(instream_t &is);
+  MaybeError parse(instream_t &is);
 
 private:
+  static constexpr std::nullopt_t ParseSuccess = std::nullopt;
   struct RawAttr {
     std::string name;
     std::string value;
@@ -21,26 +38,34 @@ private:
   enum class TagType;
   std::stack<TagType> parents;
 
-  bool parseContent(instream_t &is);
-  bool parseTag(instream_t &is);
+  bool readUntil(instream_t &is, const std::string_view &delim,
+                 /* out */ std::stringstream &os);
+  bool expect(instream_t &is, const std::string_view &expected);
+
+  MaybeError parseContent(instream_t &is);
+  MaybeError parseXMLDecl(instream_t &is);
+  MaybeError parseExclTag(instream_t &is);
+  MaybeError parseTag(instream_t &is);
   TagType parseTagType(instream_t &is);
   void dispatchTag(TagType tag, const std::vector<SVGAttribute> &attrs);
   std::string parseName(instream_t &is);
-  bool parseAttributes(instream_t &is, /*out*/ std::vector<RawAttr> &attrs);
-  static bool convertAttrs(const std::vector<RawAttr> &raw,
-                           /* out */ std::vector<SVGAttribute> &attrs);
-  bool parseAttrValue(instream_t &is, /* out */ std::string &val);
+  MaybeError parseAttributes(instream_t &is,
+                             /*out*/ std::vector<RawAttr> &attrs);
+  static MaybeError convertAttrs(const std::vector<RawAttr> &raw,
+                                 /* out */ std::vector<SVGAttribute> &attrs);
+  MaybeError parseAttrValue(instream_t &is, /* out */ std::string &val);
   void enter(TagType tag) {
     parents.push(tag);
     writer.enter();
   }
-  bool leave(TagType tag) {
+  MaybeError leave(TagType tag) {
     TagType expected = parents.top();
     if (tag != expected)
-      return false;
+      return ParseError{
+          "Encountered closing tag that has not been opened before"};
     parents.pop();
     writer.leave();
-    return true;
+    return ParseSuccess;
   }
 };
 
--- lib/svg_reader_writer.cc
+++ lib/svg_reader_writer.cc
@@ -15,56 +15,131 @@ enum class SVGReaderWriterBase::TagType {
 #include "svgutils/svg_entities.def"
 };
 
-#define PARSE_ERROR(ERR) return false
-#define PARSE_SUCCESS true
-
 // Eventually, I think I should implement
 // http://cs.lmu.edu/~ray/notes/xmlgrammar/ . However, starting out with
 // something that's 'close enough' is faster and more fun!
+using MaybeError = SVGReaderWriterBase::MaybeError;
 
-bool SVGReaderWriterBase::parse(instream_t &is) {
+MaybeError SVGReaderWriterBase::parse(instream_t &is) {
   for (is.get(Tok); !is.eof(); is.get(Tok)) {
     if (std::isspace(Tok))
       continue;
     if (Tok != '<') {
-      if (auto res = parseContent(is); !res)
-        PARSE_ERROR(res);
+      if (auto err = parseContent(is))
+        return err;
     }
     if (is.eof())
       break;
     assert(Tok == '<' && "End of content is either < or eof");
-    if (auto res = parseTag(is); !res)
-      PARSE_ERROR(res);
+    if (auto err = parseTag(is))
+      return err;
   }
   if (parents.size())
-    PARSE_ERROR("Not all tags were closed");
+    return ParseError("Not all tags were closed");
   // Required because closing tags are only written when strictly
   // necessary to allow for multiple enter()/leave() calls
   writer.finish();
-  return PARSE_SUCCESS;
+  return ParseSuccess;
 }
 
-bool SVGReaderWriterBase::parseContent(instream_t &is) {
+bool SVGReaderWriterBase::readUntil(instream_t &is,
+                                    const std::string_view &delim,
+                                    /* out */ std::stringstream &os) {
+  if (delim.empty())
+    return true;
+  if (is.eof())
+    return false;
+  size_t matchnum = 0;
+  for (is.get(Tok); !is.eof(); is.get(Tok)) {
+    os << Tok;
+    if (delim[matchnum] == Tok) {
+      ++matchnum;
+      if (matchnum == delim.size())
+        return true;
+    } else
+      matchnum = 0;
+  }
+  return matchnum == delim.size(); // In case delim is empty and 'is' is eof
+}
+bool SVGReaderWriterBase::expect(instream_t &is,
+                                 const std::string_view &expected) {
+  for (size_t i = 0; i < expected.size(); ++i) {
+    if (is.eof())
+      return false;
+    is.get(Tok);
+    if (Tok != expected[i])
+      return false;
+  }
+  return true;
+}
+
+MaybeError SVGReaderWriterBase::parseContent(instream_t &is) {
   std::stringstream content;
   content << Tok;
   for (is.get(Tok); !is.eof() && Tok != '<'; is.get(Tok))
     content << Tok;
   std::string cstr = content.str();
   if (cstr.empty())
-    return PARSE_SUCCESS;
+    return ParseSuccess;
   writer.content(cstr.c_str());
-  return PARSE_SUCCESS;
+  return ParseSuccess;
 }
-bool SVGReaderWriterBase::parseTag(instream_t &is) {
-  assert(Tok == '<' && "Expected tag to start out with <");
+
+MaybeError SVGReaderWriterBase::parseXMLDecl(instream_t &is) {
+  assert(Tok == '?' && "Expected xml declaration to start out with <?");
+  std::stringstream content;
+  if (!readUntil(is, "?>", content))
+    return ParseError("Unexpected end of input after <?");
+  assert(Tok == '>');
+  return ParseSuccess;
+}
+MaybeError SVGReaderWriterBase::parseExclTag(instream_t &is) {
+  assert(Tok == '!' && "Expected tag to start out with <!");
+  assert(!is.eof() && "Expected more content to be readable");
   is.get(Tok);
+  std::stringstream content;
+  if (Tok == '-') {
+    if (!expect(is, "-"))
+      return ParseError("Unexpected char or eof after <!-");
+    if (!readUntil(is, "-->", content))
+      return ParseError("Unexpected end of input inside comment");
+    std::string comment = content.str();
+    assert(comment.size() >= 3 &&
+           "Comment content should contain at least '-->'");
+    comment.erase(comment.size() - 3);
+    writer.comment(comment.c_str());
+  } else if (Tok == 'D') {
+    if (!expect(is, "OCTYPE"))
+      return ParseError("Expected '<!DOCTYPE' but got something different");
+    if (!readUntil(is, ">", content))
+      return ParseError("Unexpected end of input inside <!DOCTYPE> tag");
+  } else if (Tok == '[') {
+    if (!expect(is, "CDATA["))
+      return ParseError("Expected '<![CDATA[' but got something different");
+    if (!readUntil(is, "]]>", content))
+      return ParseError("Unexpected end of input inside <![CDATA[]]> tag");
+  } else
+    return ParseError("Encountered invalid sequence after <!");
+  return ParseSuccess;
+}
+
+MaybeError SVGReaderWriterBase::parseTag(instream_t &is) {
+  assert(Tok == '<' && "Expected tag to start out with <");
+  assert(!is.eof() && "Expected more content to be readable");
+  is.get(Tok);
+  if (is.eof())
+    return ParseError("Unexpected end of input after <");
+  if (Tok == '?')
+    return parseXMLDecl(is);
+  else if (Tok == '!')
+    return parseExclTag(is);
   bool isClose = Tok == '/';
   if (isClose)
     is.get(Tok);
   TagType tag = parseTagType(is);
   if (isClose) {
     if (Tok != '>')
-      PARSE_ERROR("Closing tag should end after name");
+      return ParseError("Closing tag should end after name");
     return leave(tag);
   } else if (Tok == '>') {
     dispatchTag(tag, {});
@@ -72,31 +147,31 @@ bool SVGReaderWriterBase::parseTag(instream_t &is) {
   } else if (Tok == '/') {
     is.get(Tok);
     if (Tok != '>') {
-      PARSE_ERROR("Encountered misplaced /");
+      return ParseError("Encountered misplaced /");
     }
     dispatchTag(tag, {});
   } else if (std::isspace(Tok)) {
     std::vector<RawAttr> RawAttrs;
     std::vector<SVGAttribute> Attrs;
-    if (auto res = parseAttributes(is, RawAttrs); !res)
-      PARSE_ERROR(res);
-    if (auto res = convertAttrs(RawAttrs, Attrs); !res)
-      PARSE_ERROR(res);
+    if (auto err = parseAttributes(is, RawAttrs))
+      return err;
+    if (auto err = convertAttrs(RawAttrs, Attrs))
+      return err;
     bool isClosed = Tok == '/';
     if (isClosed) {
       is.get(Tok);
       if (Tok != '>')
-        PARSE_ERROR("Encountered misplaced /");
+        return ParseError("Encountered misplaced /");
     }
     assert(Tok == '>' && "We should be on the tag's closing >");
     dispatchTag(tag, Attrs);
     if (!isClosed)
       enter(tag);
   } else if (is.eof()) {
-    PARSE_ERROR("Tag cannot end here");
+    return ParseError("Tag cannot end here");
   } else
     svg_unreachable("Somehow ended up with token not in follow-set of tag");
-  return PARSE_SUCCESS;
+  return ParseSuccess;
 }
 
 SVGReaderWriterBase::TagType SVGReaderWriterBase::parseTagType(instream_t &is) {
@@ -124,41 +199,42 @@ void SVGReaderWriterBase::dispatchTag(SVGReaderWriterBase::TagType tag,
     svg_unreachable("Cannot dispatch for special or unknown tag");
   };
 }
-bool SVGReaderWriterBase::parseAttributes(instream_t &is,
-                                          /*out*/ std::vector<RawAttr> &attrs) {
+MaybeError
+SVGReaderWriterBase::parseAttributes(instream_t &is,
+                                     /*out*/ std::vector<RawAttr> &attrs) {
   for (; !is.eof() && Tok != '>' && Tok != '/'; is.get(Tok)) {
     if (std::isspace(Tok))
       continue;
     std::string name = parseName(is);
     if (Tok != '=')
-      PARSE_ERROR("Attribute has no value");
+      return ParseError("Attribute has no value");
     is.get(Tok);
     std::string value;
-    if (auto res = parseAttrValue(is, value); !res)
-      PARSE_ERROR(res);
+    if (auto err = parseAttrValue(is, value))
+      return err;
     attrs.emplace_back();
     attrs.back().name = std::move(name);
     attrs.back().value = std::move(value);
   }
   if (is.eof() && Tok != '>' && Tok != '/')
-    PARSE_ERROR("Unexpected end of input");
-  return PARSE_SUCCESS;
+    return ParseError("Unexpected end of input");
+  return ParseSuccess;
 }
 
-bool SVGReaderWriterBase::parseAttrValue(instream_t &is,
-                                         /* out */ std::string &val) {
+MaybeError SVGReaderWriterBase::parseAttrValue(instream_t &is,
+                                               /* out */ std::string &val) {
   char Delim = 0;
   if (Tok == '"' || Tok == '\'')
     Delim = Tok;
   if (!Delim)
-    PARSE_ERROR("Attribute value not starting with ' or \"");
+    return ParseError("Attribute value not starting with ' or \"");
   std::stringstream value;
   for (is.get(Tok); !is.eof() && Tok != Delim; is.get(Tok))
     value << Tok; // FIXME check for illegal characters
   if (is.eof() && Tok != Delim)
-    PARSE_ERROR("Attribute value not starting with ' or \"");
+    return ParseError("Attribute value not starting with ' or \"");
   val = value.str();
-  return PARSE_SUCCESS;
+  return ParseSuccess;
 }
 std::string SVGReaderWriterBase::parseName(instream_t &is) {
   std::stringstream namestr;
@@ -169,9 +245,9 @@ std::string SVGReaderWriterBase::parseName(instream_t &is) {
   return namestr.str();
 }
 
-bool SVGReaderWriterBase::convertAttrs(
-    const std::vector<RawAttr> &raws,
-    /* out */ std::vector<SVGAttribute> &attrs) {
+MaybeError
+SVGReaderWriterBase::convertAttrs(const std::vector<RawAttr> &raws,
+                                  /* out */ std::vector<SVGAttribute> &attrs) {
   assert(attrs.empty() && "Expected output vector to be empty");
   for (const RawAttr &raw : raws) {
 #define SVG_ATTR(NAME, STR, DEFAULT)                                           \
@@ -180,7 +256,7 @@ bool SVGReaderWriterBase::convertAttrs(
     continue;                                                                  \
   }
 #include "svgutils/svg_entities.def"
-    PARSE_ERROR("Unknown attribute");
+    return ParseError("Unknown attribute");
   }
-  return PARSE_SUCCESS;
+  return ParseSuccess;
 }
